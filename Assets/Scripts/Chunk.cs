f44c1ef4b6b48707ecceb473008fd8561b9b6309
Remludar <jason.ewton@gmail.com>
2018-07-01 12:33:58 -0500

    Initial Commit

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public const int WIDTH =  16;
+    public const int DEPTH =  16;
+    public const int HEIGHT = 16;
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+    List<Vector3> verts = new List<Vector3>();
+    List<int> tris = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    int numTris = 0;
+
+    GameObject terrainGO;
+    MyTerrain terrain;
+    Vector3 position;
+
+    //Overloads
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        position = new Vector3(transform.position.x / WIDTH, transform.position.y / HEIGHT, transform.position.z / DEPTH);
+        terrainGO = GameObject.Find("Terrain");
+        terrain = terrainGO.GetComponent<MyTerrain>();
+        Render();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Render();
+    }
+
+    //Helpers
+    void TestTerrain()
+    {
+        var grids = new MarchingCubes.GridCell[WIDTH * HEIGHT * DEPTH];
+
+        var mc = new MarchingCubes();
+        var triangles = new MarchingCubes.Triangle[5];
+        var triCount = 0;
+
+        for (int z = 0; z < DEPTH; z++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                for (int x = 0; x < WIDTH; x++)
+                {
+                    var gridCell = new MarchingCubes.GridCell();
+                    gridCell.p[0] = new Vector3(x + 0, y + 0, z + 1);
+                    gridCell.p[1] = new Vector3(x + 1, y + 0, z + 1);
+                    gridCell.p[2] = new Vector3(x + 1, y + 0, z + 0);
+                    gridCell.p[3] = new Vector3(x + 0, y + 0, z + 0);
+                    gridCell.p[4] = new Vector3(x + 0, y + 1, z + 1);
+                    gridCell.p[5] = new Vector3(x + 1, y + 1, z + 1);
+                    gridCell.p[6] = new Vector3(x + 1, y + 1, z + 0);
+                    gridCell.p[7] = new Vector3(x + 0, y + 1, z + 0);
+
+                    gridCell.val[0] = MyTerrain.noise[((int)position.x * WIDTH + x + 0) + ((int)position.y * HEIGHT + y + 0) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 1) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[1] = MyTerrain.noise[((int)position.x * WIDTH + x + 1) + ((int)position.y * HEIGHT + y + 0) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 1) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[2] = MyTerrain.noise[((int)position.x * WIDTH + x + 1) + ((int)position.y * HEIGHT + y + 0) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 0) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[3] = MyTerrain.noise[((int)position.x * WIDTH + x + 0) + ((int)position.y * HEIGHT + y + 0) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 0) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[4] = MyTerrain.noise[((int)position.x * WIDTH + x + 0) + ((int)position.y * HEIGHT + y + 1) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 1) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[5] = MyTerrain.noise[((int)position.x * WIDTH + x + 1) + ((int)position.y * HEIGHT + y + 1) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 1) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[6] = MyTerrain.noise[((int)position.x * WIDTH + x + 1) + ((int)position.y * HEIGHT + y + 1) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 0) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+                    gridCell.val[7] = MyTerrain.noise[((int)position.x * WIDTH + x + 0) + ((int)position.y * HEIGHT + y + 1) * MyTerrain.NOISE_WIDTH + ((int)position.z * DEPTH + z + 0) * MyTerrain.NOISE_HEIGHT * MyTerrain.NOISE_WIDTH];
+
+                    grids[x + y * WIDTH + z * HEIGHT * WIDTH] = gridCell;
+                }
+            }
+        }
+
+        for (int i = 0; i < grids.Length; i++)
+        {
+            triangles = new MarchingCubes.Triangle[5];
+            for (int k = 0; k < triangles.Length; k++)
+            {
+                triangles[k] = new MarchingCubes.Triangle();
+            }
+
+            numTris = mc.Polygonize(grids[i], terrain.isolevel, triangles);
+            for (int j = 0; j < numTris; j++)
+            {
+                verts.Add(triangles[j].p[0]);
+                verts.Add(triangles[j].p[1]);
+                verts.Add(triangles[j].p[2]);
+
+                tris.Add(triCount * 3 + 0);
+                tris.Add(triCount * 3 + 1);
+                tris.Add(triCount * 3 + 2);
+
+                triCount++;
+            }
+        }
+    }
+    void Render()
+    {
+        verts.Clear();
+        tris.Clear();
+        uvs.Clear();
+        meshFilter.mesh.Clear();
+
+        TestTerrain();
+
+
+        var mesh = meshFilter.mesh;
+        mesh.vertices = verts.ToArray();
+        mesh.triangles = tris.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Scripts/MyTerrain.cs
@@ -0,0 +1,129 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTerrain : MonoBehaviour {
+
+    public const int WIDTH =  5;
+    public const int DEPTH =  5;
+    public const int HEIGHT = 5;
+
+    public static double min = double.MaxValue;
+    public static double max = double.MinValue;
+
+    public const int NOISE_WIDTH = (WIDTH * (Chunk.WIDTH + 1)); 
+    public const int NOISE_HEIGHT = (HEIGHT * (Chunk.HEIGHT + 1)); 
+    public const int NOISE_DEPTH = (DEPTH * (Chunk.DEPTH + 1)); 
+    public static double[] noise = new double[NOISE_WIDTH * NOISE_HEIGHT * NOISE_DEPTH];
+
+    public float isolevel;
+
+    //Overloads
+    void Start()
+    {
+        //Generate2DNoise();
+        Generate3DNoise();
+        GenerateChunks();
+    }
+
+    //Helpers
+    private void Generate2DNoise()
+    {
+        int index = 0;
+
+        for (int z = 0; z < NOISE_DEPTH; z++)
+        {
+            for (int y = 0; y < NOISE_HEIGHT; y++)
+            {
+                for (int x = 0; x < NOISE_WIDTH; x++)
+                {
+                    var octaves = 3;
+                    var deltaFrequency = 1.5f;
+                    var deltaAmplitude = 0.85f;
+                    var deltaScale = 1.0;
+
+                    var frequency = 0.35f;
+                    var amplitude = 100.0;
+                    var scale = 1.0;
+
+
+                    double sample = 0.0;
+                    for (int i = 0; i < octaves; i++)
+                    {
+
+                        sample += Mathf.PerlinNoise((x / (float)Chunk.WIDTH) * frequency,
+                                                    (z / (float)Chunk.DEPTH) * frequency
+                                                    ) * amplitude * scale - y * 1.5f;
+
+                        frequency *= deltaFrequency;
+                        amplitude *= deltaAmplitude;
+                        scale *= deltaScale;
+                    }
+
+                    if (sample < min) min = sample;
+                    if (sample > max) max = sample;
+
+                    index = x + y * NOISE_WIDTH + z * NOISE_HEIGHT * NOISE_WIDTH;
+                    noise[index] = sample;
+
+                }
+            }
+        }
+    }
+    private void Generate3DNoise()
+    {
+        var simplexNoise = new OpenSimplexNoise();
+     
+        for (int z = 0; z < NOISE_DEPTH; z++)
+        {
+            for (int y = 0; y < NOISE_HEIGHT; y++)
+            {
+                for (int x = 0; x < NOISE_WIDTH; x++)
+                {
+                    var octaves = 3;
+                    var deltaFrequency = 2.0;
+                    var deltaAmplitude = 1.0;
+                    var deltaScale = 1.0;
+
+                    var frequency = 0.75;
+                    var amplitude = 10.0;
+                    var scale = 1.0;
+
+                    double sample = 0.0;
+                    for (int i = 0; i < octaves; i++)
+                    {
+                        
+                        sample += simplexNoise.eval((x / (double)(1 * Chunk.WIDTH)) * frequency,
+                                                (y / (double)(1 * Chunk.HEIGHT)) * frequency,
+                                                (z / (double)(1 * Chunk.DEPTH)) * frequency) * amplitude * scale -y * 0.5f;
+
+                        frequency *= deltaFrequency;
+                        amplitude *= deltaAmplitude;
+                        scale *= deltaScale;
+                    }
+
+                    if (sample < min) min = sample;
+                    if (sample > max) max = sample;
+
+                    var index = x + y * NOISE_WIDTH + z * NOISE_HEIGHT * NOISE_WIDTH;
+                    noise[index] = sample;
+                }
+            }
+        }
+    }
+    
+private void GenerateChunks()
+{
+    for (int z = 0; z < DEPTH; z++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                for (int x = 0; x < WIDTH; x++)
+                {
+                    var chunkGO = Instantiate(Resources.Load("Prefabs/Chunk"), new Vector3(x * Chunk.WIDTH, y * Chunk.HEIGHT, z * Chunk.DEPTH), Quaternion.identity) as GameObject;
+                }
+            }
+        }
+    }
+}
